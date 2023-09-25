4b36f3c24bce926296b99632615d89ba8d6d7c2e
tamii2004 <simmu.amarok@gmail.com>
2023-09-25 12:49:23 -0300

    proyecto listo

--- app1/views.py
+++ app1/views.py
@@ -5,6 +5,9 @@ def vista1(request):
     html ="""
 <h1>bienvenido</h1>
 <h2>esta en la vista 1</h2>
+<a href="/vista2/" > ir a la vista 2</a>
+<a href="/vista3/" > ir a la vista 3</a>
+<a href="/vista4/" > ir a la vista 4</a>
 
 """
     return HttpResponse(html)
@@ -13,6 +16,9 @@ def vista2(request):
     html ="""
 <h1>bienvenido</h1>
 <h2>esta en la vista 2</h2>
+<a href="/" > ir a la vista 1</a>
+<a href="/vista3/" > ir a la vista 3</a>
+<a href="/vista4/" > ir a la vista 4</a>
 
 """
     return HttpResponse(html)
